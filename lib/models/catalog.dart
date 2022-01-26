11165c54218440e05affa568af1e31795983336d
Taimoor Hussain <82362396+taimoorKVD@users.noreply.github.com>
2022-01-26 22:36:50 +0500

    dynamic data added into cart

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -3,6 +3,12 @@
 import 'package:flutter/cupertino.dart';
 
 class CatalogModel {
+  static final catModel = CatalogModel._internal();
+
+  CatalogModel._internal();
+
+  factory CatalogModel() => catModel;
+
   static List<Item> items = [];
 
  Item getById(int id) =>
