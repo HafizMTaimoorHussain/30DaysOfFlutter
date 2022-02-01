bc14bf2987c79022ee5f9b5bcc87b75bbc278468
Taimoor Hussain <82362396+taimoorKVD@users.noreply.github.com>
2022-02-01 21:49:29 +0500

    converted all the statefulwidget into statelesswidget

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -55,14 +55,7 @@ class _CarTotal extends StatelessWidget {
   }
 }
 
-class _CartList extends StatefulWidget {
-  const _CartList({Key? key}) : super(key: key);
-
-  @override
-  _CartListState createState() => _CartListState();
-}
-
-class _CartListState extends State<_CartList> {
+class _CartList extends StatelessWidget{
   final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
@@ -74,7 +67,7 @@ class _CartListState extends State<_CartList> {
             icon: Icon(Icons.remove_circle_outline),
             onPressed: () {
               _cart.remove(_cart.items[index]);
-              setState(() {});
+              // setState(() {});
             },
           ),
           title: _cart.items[index].name.text.make(),
